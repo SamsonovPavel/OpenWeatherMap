83ca7895ba09f4bcc17d9332c4a310a4ba353b0d
Pavel Samsonov <samsonovpavel@mail.ru>
2019-09-17 18:56:17 +0300

    Initial project

---
+++ OpenWeatherMap/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  OpenWeatherMap
+//
+//  Created by psamsonov on 17/09/2019.
+//  Copyright © 2019 MegaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
