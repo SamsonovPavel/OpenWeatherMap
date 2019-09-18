b045cdba5e4150daa43ed77cd9bdd62c202e50a7
Pavel Samsonov <samsonovpavel@mail.ru>
2019-09-18 15:56:44 +0300

    setup tab bar

--- OpenWeatherMap/Clsasses/TabBarController.swift
+++ OpenWeatherMap/Clsasses/TabBarController.swift
@@ -14,12 +14,30 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupTabBar()
+    }
+}
+
+extension TabBarController {
+    
+    private func setupTabBar() {
+        
+        let images = ["weather", "history", "map"]
+        let selectedImages = ["weather_selected", "history_selected", "map_selected"]
+        
         for (i, _) in (viewControllers?.enumerated())! {
+            
             let item = tabBar.items![i]
+            
+            item.image = UIImage(named: images[i])
             item.image = item.image?.withRenderingMode(.alwaysOriginal)
-            item.image = UIImage(named: "weather")
+            item.selectedImage = UIImage(named: selectedImages[i])
+            item.selectedImage = item.selectedImage?.withRenderingMode(.alwaysOriginal)
+            
+            item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.3)], for: .normal)
+            item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 1)], for: .selected)
         }
     }
 }
